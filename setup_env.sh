885aa60a6215fab6af66f2974ed93433b3322171
Henry Tang <tq.quanghuy@gmail.com>
2026-05-03 16:45:20 +0900

    Handled a particular error.

--- setup_env.sh
+++ setup_env.sh
@@ -3,9 +3,25 @@ apt update && apt upgrade -y
 
 # Install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source "$HOME/.local/bin/env"
 
 # Install croc
 curl https://getcroc.schollz.com | bash
 
 # Install tmux
 apt install tmux -y
+
+# Pull project
+git clone https://github.com/henrytang05/hw4.git
+
+# Sync uv
+uv sync
+
+# Install torch GPU
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+# Install project
+uv pip install -e .
+
+# source environment
+source .venv/bin/activate
